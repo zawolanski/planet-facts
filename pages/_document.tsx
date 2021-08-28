b31d6827896c0d4797bb1dc1a4e1f3d2ca5cd26f
Sebastian <sebastian.zawolanski@gmail.com>
2021-08-28 13:36:57 +0200

    Change theme color

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -41,6 +41,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Spartan:wght@400;700&display=swap"
             rel="stylesheet"
           ></link>
+          <meta name="theme-color" content="#070724" />
         </Head>
         <body>
           <Main />
